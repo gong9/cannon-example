e1c5ce666436822d3bc0a63ff28ac851d54e0399
gongzhen <517441540@qq.com>
2024-06-01 15:46:28 +0800

    feat: add a car model

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import { Clock, Color, DoubleSide, Mesh, MeshBasicMaterial, PlaneGeometry, SceneControl as Scene, SphereGeometry, Vector3, use } from '@anov/3d-core'
-import { Body, ContactMaterial, Material, Plane, Sphere, Vec3, World } from 'cannon-es'
+import { Clock, Color, DoubleSide, Mesh, MeshBasicMaterial, ModelLoader, PlaneGeometry, SceneControl as Scene, SphereGeometry, Vector3, use } from '@anov/3d-core'
+import { Body, Box, ContactMaterial, Material, Plane, Sphere, Vec3, World } from 'cannon-es'
 import CannonDebugger from 'cannon-es-debugger'
+import { ShapeType, threeToCannon } from 'three-to-cannon'
+
+const load = new ModelLoader()
 
 const scene = new Scene({
   orbitControls: {
@@ -30,7 +33,6 @@ const cannonDebugger = new CannonDebugger(scene, world, {
 })
 
 // threejs世界
-
 const planeGeometry = new PlaneGeometry(100, 100)
 const material = new MeshBasicMaterial({ color: new Color('#ccc'), side: DoubleSide })
 
@@ -63,7 +65,7 @@ const sphereBody = new Body({
   material: plasticMaterial,
 })
 
-world.addBody(sphereBody)
+// world.addBody(sphereBody)
 
 const planeShape = new Plane()
 
@@ -85,21 +87,51 @@ planeBody.quaternion.setFromAxisAngle(new Vec3(1, 0, 0), -Math.PI / 2)
 
 world.addBody(planeBody)
 
-sphereBody.applyLocalForce(new Vec3(10, 0, 1000), new Vec3(0, 0, 0))
+sphereBody.applyLocalForce(new Vec3(20, 0, 200), new Vec3(0, 0, 0))
 
 const clock = new Clock()
 let oldElapsedTime = 0
+
+let car
+let carBody
+
+load.loadGLTF('./car.glb', false, false, '', (gltf) => {
+  car = gltf.scene
+  car.position.set(0, 10, 0)
+
+  const boxShape = new Box(
+    new Vec3(1, 0.5, 1),
+  )
+
+  carBody = new Body({
+    mass: 1,
+    position: new Vec3(0, 10, 0),
+    shape: boxShape,
+    material: plasticMaterial,
+  })
+
+  scene.add(car)
+  world.addBody(carBody)
+
+  return gltf
+})
 
 use.useframe(() => {
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - oldElapsedTime
   oldElapsedTime = elapsedTime
 
-  //   sphereBody.applyForce(new Vec3(-10.5, 0, 0), sphereBody.position)
+  // sphereBody.applyForce(new Vec3(-10.5, 0, 0), sphereBody.position)
 
   world.step(1 / 60, deltaTime, 3)
 
+  if (car && carBody) {
+    car.position.copy(carBody.position as any)
+    car.quaternion.copy(carBody.quaternion as any)
+  }
+
   sphere.position.copy(sphereBody.position as any)
   sphere.quaternion.copy(sphereBody.quaternion as any)
+
   cannonDebugger.update()
 })
